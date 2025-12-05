1983766b2c2087d3342b112f3c9415dbd7655e2a
Asmit Nayak <asmit.nayak@gmail.com>
2025-12-05 05:13:57 -0600

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 // TODO: Replace 'YOUR_USERNAME' with your GitHub username
 export default defineConfig({
-  site: 'https://YOUR_USERNAME.github.io',
-  base: '/wipi_website/',
+  site: 'https://wiscprivacy.com',
+  base: '/new_wipi_website/',
   output: 'static',
   build: {
     assets: 'assets'
